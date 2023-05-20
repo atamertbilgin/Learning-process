7c018b5f3c229915e54f95ee02ed783c6917e6f3
atamertbilgin <atamertbilgin@gmail.com>
2023-05-20 10:21:23 +0300

    added first devops terraform project

---
+++ Devops/202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/var.tf
@@ -0,0 +1,16 @@
+variable "git-name" {
+  default = "atamertbilgin"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "first-key"
+}
+
+variable "hosted-zone" {
+  default = "atamertbilgin.com."
+}
+
